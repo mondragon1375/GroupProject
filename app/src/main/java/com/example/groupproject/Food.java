d0f1a56c336a7358961dcac3522cd1699d96c29f
mondragon1375 <mondragon1375@gmail.com>
2019-10-04 12:56:55 -0500

    food

--- app/src/main/java/com/example/groupproject/Food.java
+++ app/src/main/java/com/example/groupproject/Food.java
@@ -4,50 +4,29 @@ public class Food {
     private String foodName;
     private String description;
     private double price;
-    private int imageResourceID;
+    //private int imageResourceID;
 
-    public Food(String foodName, String description, double price, int imageResourceID){
+    public Food(String foodName, String description, double price){
         this.foodName = foodName;
         this.description = description;
         this.price = price;
-        this.imageResourceID = imageResourceID;
+        //this.imageResourceID = imageResourceID;
     }
 
     public static final Food[] drinksFood = {
-            new Food("Sample Food", "Sample Description", 3.99, R.drawable.circle),
-            new Food("Pancakes", "3 pancakes, choice of meat, potato", 7.99, R.drawable.circle)
+            new Food("Sample Food", "Sample Description", 3.99)
     };
 
-    public String toString(){
-        return foodName;
-    }
+    public String toString(){ return foodName; }
 
-    public String getFoodName() {
-        return foodName;
-    }
+    public String getFoodName() { return foodName; }
 
-    public void setFoodName(String foodName) {
-        this.foodName = foodName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public String getDescription() { return description; }
 
     public double getPrice() { return price; }
 
     public void setPrice(double price) { this.price = price; }
 
-    public int getImageResourceID() {
-        return imageResourceID;
-    }
-
-    public void setImageResourceID(int imageResourceID) {
-        this.imageResourceID = imageResourceID;
-    }
+    //public int getImageResourceID() { return imageResourceID;}
 
 }
