3a12f8bca67dc8f5cc2fbe6844ada98ebf079b15
simhadri4025 <simhadri4025@students.d211.org>
2019-10-09 13:30:35 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/groupproject/Food.java

--- app/src/main/java/com/example/groupproject/AdultsMenuActivity.java
+++ app/src/main/java/com/example/groupproject/AdultsMenuActivity.java
@@ -1,7 +1,13 @@
 package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class AdultsMenuActivity extends AppCompatActivity {
 
@@ -10,6 +16,4 @@ public class AdultsMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adults_menu);
     }
-
-
 }
--- app/src/main/java/com/example/groupproject/DrinksMenuActivity.java
+++ app/src/main/java/com/example/groupproject/DrinksMenuActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DrinksMenuActivity extends AppCompatActivity {
 
@@ -12,7 +16,28 @@ public class DrinksMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks_menu);
         Intent intent = getIntent();
-    }
 
+        ArrayAdapter<Food> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, Food.drinksFood);
+
+        ListView listFoods = (ListView) findViewById(R.id.list_drinkChoices);
+        listFoods.setAdapter(listAdapter);
+
+        // Create a listener to listen for when a Food item is clicked on
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> listFoods,
+                                            View itemView, int position, long id) {
+
+
+
+                        // Pass the Food name the user clicks on to BreakfastChoicesActivity
+                    }
+                };
+        // Assign the listener to the list view
+        listFoods.setOnItemClickListener(itemClickListener);
+
+
+    }
 
 }
